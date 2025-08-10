00b1f47c578891bfb079fee63671e02d0a4a4f99
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2025-08-10 18:44:21 +0530

    🗑️ del: unwanted textures

--- src/assetSources.js
+++ src/assetSources.js
@@ -1,37 +1,37 @@
 const ASSETS = [
     {
         id: 'grassTexture',
-        path: ['/assets/textures/blocks/grass/grass_2.png'],
+        path: ['/assets/textures/blocks/grass/grass.png'],
         type: 'texture'
     },
     {
         id: 'grassTextureSide',
-        path: ['/assets/textures/blocks/grass/grass_block_side_2.png'],
+        path: ['/assets/textures/blocks/grass/grass_side.png'],
         type: 'texture'
     },
     {
         id: 'dirtTexture',
-        path: ['/assets/textures/blocks/dirt/dirt_2.png'],
+        path: ['/assets/textures/blocks/dirt/dirt.png'],
         type: 'texture'
     },
     {
         id: 'stoneTexture',
-        path: ['/assets/textures/blocks/stone/cobbled_deepslate_2.png'],
+        path: ['/assets/textures/blocks/stone/stone.png'],
         type: 'texture'
     },
     {
         id: 'coalOreTexture',
-        path: ['/assets/textures/blocks/coal/coal_ore_x.png'],
+        path: ['/assets/textures/blocks/coal/coal_ore.png'],
         type: 'texture'
     },
     {
         id: 'ironOreTexture',
-        path: ['/assets/textures/blocks/iron/iron_ore_1.png'],
+        path: ['/assets/textures/blocks/iron/iron_ore.png'],
         type: 'texture'
     },
     {
         id: 'goldOreTexture',
-        path: ['/assets/textures/blocks/gold/gold_ore_1.png'],
+        path: ['/assets/textures/blocks/gold/gold_ore.png'],
         type: 'texture'
     }
 ];
